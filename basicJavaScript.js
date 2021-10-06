0a94a08d71cd9568ef2b50e0ca6586f1f954e012
dasiklasik <daryasamsonowicz@gmail.com>
2021-10-06 22:18:04 +0300

    feat: add new completed task

--- basicJavaScript.js
+++ basicJavaScript.js
@@ -1,2 +1,12 @@
 /*Initializing Variables with the Assignment OperatorPassed */
 var a = 9;
+
+/*Shopping List */
+var myList = [];
+myList.push(
+  ["Chocolate Bar", 15],
+  ["Banana", 2],
+  ["Crocodile", 1],
+  ["Ice-cream", 10],
+  ["Spagetti", 3]
+);
