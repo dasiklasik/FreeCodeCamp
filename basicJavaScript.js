6b2dae1c43ac5458b3a44cfb268559bdcc1d9a90
dasiklasik <daryasamsonowicz@gmail.com>
2021-10-07 12:27:32 +0300

    feat: add completed task

--- basicJavaScript.js
+++ basicJavaScript.js
@@ -31,12 +31,22 @@ function addThree() {
   sum = sum + 3;
 }
 
-// Only change code below this line
 function addFive() {
   sum += 5;
 }
-
-// Only change code above this line
-
 addThree();
 addFive();
+
+/*Stand in Line */
+
+function nextInLine(arr, item) {
+  arr.push(item);
+  return item;
+}
+
+var testArr = [1, 2, 3, 4, 5];
+
+// Display code
+console.log("Before: " + JSON.stringify(testArr));
+console.log(nextInLine(testArr, 6));
+console.log("After: " + JSON.stringify(testArr));
