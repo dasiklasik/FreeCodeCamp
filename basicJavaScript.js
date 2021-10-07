ef33f477a0bd62e1e7341632daf6759c21c4cbdf
dasiklasik <daryasamsonowicz@gmail.com>
2021-10-07 17:21:05 +0300

    feat: add completed task

--- basicJavaScript.js
+++ basicJavaScript.js
@@ -132,3 +132,12 @@ function testLogicalAnd(val) {
 
   return "No";
 }
+
+/*Comparisons with the Logical Or Operator */
+function testLogicalOr(val) {
+  if (val < 10 || val > 20) {
+    return "Outside";
+  }
+
+  return "Inside";
+}
