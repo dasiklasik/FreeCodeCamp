8d3934474d3138a56fda10d878f1dba49c1b7b6c
dasiklasik <daryasamsonowicz@gmail.com>
2021-10-07 16:42:36 +0300

    feat: add complete task

--- basicJavaScript.js
+++ basicJavaScript.js
@@ -46,12 +46,19 @@ function nextInLine(arr, item) {
 
 /*Use Conditional Logic with If Statements */
 function trueOrFalse(wasThatTrue) {
-  // Only change code below this line
   if (wasThatTrue) {
     return "Yes, that was true";
   } else {
     return "No, that was false";
   }
+}
 
-  // Only change code above this line
+/*Comparison with the Equality Operator */
+// Setup
+function testEqual(val) {
+  if (val == 12) {
+    // Change this line
+    return "Equal";
+  }
+  return "Not Equal";
 }
