bde47ee010cfccd56a1e48f54b0c66b18b4be90a
dasiklasik <daryasamsonowicz@gmail.com>
2021-10-07 17:09:28 +0300

    feat: add completed task

--- basicJavaScript.js
+++ basicJavaScript.js
@@ -83,3 +83,16 @@ function testGreaterThan(val) {
 
   return "10 or Under";
 }
+
+/*Comparison with the Greater Than Or Equal To Operator */
+function testGreaterOrEqual(val) {
+  if (val >= 20) {
+    return "20 or Over";
+  }
+
+  if (val >= 10) {
+    return "10 or Over";
+  }
+
+  return "Less than 10";
+}
