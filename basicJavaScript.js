0002cf3c8f99960d4d1ed40ebca25dc0d5f4589a
dasiklasik <daryasamsonowicz@gmail.com>
2021-10-08 12:04:28 +0300

    feat: add completed task

--- basicJavaScript.js
+++ basicJavaScript.js
@@ -189,3 +189,23 @@ function golfScore(par, strokes) {
 
   return "Change Me";
 }
+
+/*Selecting from Many Options with Switch Statements */
+function caseInSwitch(val) {
+  var answer = "";
+  switch (val) {
+    case 1:
+      answer = "alpha";
+      break;
+    case 2:
+      answer = "beta";
+      break;
+    case 3:
+      answer = "gamma";
+      break;
+    case 4:
+      answer = "delta";
+      break;
+  }
+  return answer;
+}
