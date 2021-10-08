b013628da4a9550b2174cb672dd0c2c2ed9dbe25
dasiklasik <daryasamsonowicz@gmail.com>
2021-10-08 20:22:20 +0300

    feat: add completed task

--- basicJavaScript.js
+++ basicJavaScript.js
@@ -292,3 +292,38 @@ function abTest(a, b) {
 
   return Math.round(Math.pow(Math.sqrt(a) + Math.sqrt(b), 2));
 }
+
+/*Counting Cards */
+var count = 0;
+
+function cc(card) {
+  switch (card) {
+    case 2:
+    case 3:
+    case 4:
+    case 5:
+    case 6:
+      count += 1;
+      break;
+    case 7:
+    case 8:
+    case 9:
+      break;
+    case 10:
+    case "J":
+    case "Q":
+    case "K":
+    case "A":
+      count -= 1;
+      break;
+  }
+  let res = "";
+
+  if (count > 0) {
+    res = "Bet";
+  } else {
+    res = "Hold";
+  }
+
+  return count + " " + res;
+}
