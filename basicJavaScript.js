c4aae0d08f8860ba56efac59876b34da4ab592a9
dasiklasik <daryasamsonowicz@gmail.com>
2021-10-08 20:08:51 +0300

    feat: add completed task

--- basicJavaScript.js
+++ basicJavaScript.js
@@ -254,3 +254,27 @@ function sequentialSizes(val) {
 
   return answer;
 }
+
+/*Replacing If Else Chains with Switch */
+function chainToSwitch(val) {
+  var answer = "";
+  switch (val) {
+    case "bob":
+      answer = "Marley";
+      break;
+    case 42:
+      answer = "The Answer";
+      break;
+    case 1:
+      answer = "There is no #1";
+      break;
+    case 99:
+      answer = "Missed me by this much!";
+      break;
+    case 7:
+      answer = "Ate Nine";
+      break;
+  }
+
+  return answer;
+}
