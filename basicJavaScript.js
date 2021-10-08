dcdceea6da415c76f4c5182d86c0a9b2b30e58c1
dasiklasik <daryasamsonowicz@gmail.com>
2021-10-08 21:32:24 +0300

    feat: add completed task

--- basicJavaScript.js
+++ basicJavaScript.js
@@ -345,3 +345,14 @@ var testObj = {
 
 var playerNumber = 16;
 var player = testObj[playerNumber];
+
+/*Updating Object Properties */
+
+var myDog = {
+  name: "Coder",
+  legs: 4,
+  tails: 1,
+  friends: ["freeCodeCamp Campers"],
+};
+
+myDog["name"] = "Happy Coder";
